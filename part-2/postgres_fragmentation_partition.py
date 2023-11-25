3910d1f21a258aa1a611426cfd3a19aaf85fb4cd
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2023-11-24 22:51:09 -0700

    all parts

--- part-2/postgres_fragmentation_partition.py
+++ part-2/postgres_fragmentation_partition.py
@@ -19,11 +19,12 @@ import psycopg2
 from psycopg2 import extensions
 import random
 import logging
+from prettytable import PrettyTable
 
 # logging.basicConfig(level=logging.INFO)
 print("hello tables...")
 
-def make_order(conn, zip_codes):
+def create_partition_on_order_table(conn, zip_codes):
     print("Creating Order table...")
     try:
         print("Creating Order table...")
@@ -31,10 +32,10 @@ def make_order(conn, zip_codes):
         zip_codes = min(100, zip_codes)
         cur = conn.cursor()
         table_creation_query = f"""
-        CREATE TABLE {ORDER}(
+         CREATE TABLE {ORDER}(
             {CUSTOMER_ID} INT NOT NULL,
-            {STATUS} VARCHAR(10) NOT NULL,
-            {ORDER_ID} VARCHAR(10),
+            {STATUS} VARCHAR(20) NOT NULL,
+            {ORDER_ID} INT,
             {ZIPCODE} INT NOT NULL,
             {AGENT_ID} INT
         ) PARTITION BY LIST ({ZIPCODE});"""
@@ -42,21 +43,17 @@ def make_order(conn, zip_codes):
      
         create_sequence_query = f"CREATE SEQUENCE order_id_seq START 9000;"
         cur.execute(create_sequence_query)
-        print(table_creation_query)
         cur.execute(table_creation_query)
         zip_c =  85200
         for i in range (0, zip_codes):
             partition_creation_query = f"CREATE TABLE Order_zip_code_{zip_c+i} PARTITION OF {ORDER} FOR VALUES IN ({zip_c+i});"
             cur.execute(partition_creation_query)
-            # create index if not exists
-            index_creation_query = f"CREATE INDEX IF NOT EXISTS Order_zip_code_{zip_c+i}_index ON Order_zip_code_{zip_c+i}  ({ORDER_ID});"
-            cur.execute(index_creation_query)
         conn.commit()
     except Exception as e:
         logging.error(f"Error creating Order table: {e}")
 
 
-def make_order_item(conn, zip_codes):
+def create_partition_on_orderitem_table(conn, zip_codes):
     print("Creating Order_Item table...")
     try:
         print("Creating Order_Item table...")
@@ -65,24 +62,16 @@ def make_order_item(conn, zip_codes):
         cur = conn.cursor()
         table_creation_query = f"""
         CREATE TABLE {ORDER_ITEM}(
-            {ORDER_ID} VARCHAR(10),
+            {ORDER_ID} INT,
             {MED_ID} INT NOT NULL,
             {QUANTITY} INT NOT NULL,
             {ZIPCODE} INT NOT NULL
         ) PARTITION BY LIST ({ZIPCODE});"""
         cur.execute(table_creation_query)
-
-        #create index if not exists
-        index_creation_query = f"CREATE INDEX IF NOT EXISTS {ORDER_ITEM}_zip_code_index ON {ORDER_ITEM} ({ZIPCODE});"
-        cur.execute(index_creation_query)
         zip_c =  85200
         for i in range (0, zip_codes):
             partition_creation_query = f"CREATE TABLE Order_Item_zip_code_{zip_c+i} PARTITION OF {ORDER_ITEM} FOR VALUES IN ({zip_c+i});"
             cur.execute(partition_creation_query)
-            # create index if not exists
-            index_creation_query = f"CREATE INDEX IF NOT EXISTS Order_Item_zip_code_{zip_c+i}_index ON Order_Item_zip_code_{zip_c+i} ({ORDER_ID});"
-            cur.execute(index_creation_query)
-
         conn.commit()
     except Exception as e:
         logging.error(f"Error creating Order_Item table: {e}")
@@ -100,7 +89,7 @@ def create_database(dbname, conn):
     conn.close()
 
 
-def make_delivery_agent(conn, zip_codes):
+def create_partition_on_deliveryagent_table(conn, zip_codes):
     print("Creating Delivery_Agent table...")
     zip_codes = min(100, zip_codes)
 
@@ -109,29 +98,21 @@ def make_delivery_agent(conn, zip_codes):
     CREATE TABLE {DELIVERY_AGENT_TABLE}(
         {AGENT_ID} SERIAL,
         {AGENT_NAME} VARCHAR(255) NOT NULL,
-        {ORDER_ID} VARCHAR(10),
+        {ORDER_ID} INT,
         {ZIPCODE} INT NOT NULL
     ) PARTITION BY LIST ({ZIPCODE});"""
 
     cur.execute(table_creation_query)
-
-    index_creation_query = f"CREATE INDEX IF NOT EXISTS Delivery_Agent_zip_code_index ON Delivery_Agent ({ZIPCODE});"   
-    cur.execute(index_creation_query)
-
     zip_c =  85200
     for i in range (0, zip_codes):
         partition_creation_query = f"CREATE TABLE Delivery_Agent_zip_code_{zip_c+i} PARTITION OF Delivery_Agent FOR VALUES IN ({zip_c+i});"
         cur.execute(partition_creation_query)
-        # create index if not exists
-        index_creation_query = f"CREATE INDEX IF NOT EXISTS Delivery_Agent_zip_code_{zip_c+i}_index ON Delivery_Agent_zip_code_{zip_c+i}({ORDER_ID});"
-        cur.execute(index_creation_query)
-
     conn.commit()
 
 
 def insert_data_agent(conn, zip_codes):
     print("Inserting data into Delivery_Agent table...")
-    zip_codes = min(2, zip_codes)
+    zip_codes = min(100, zip_codes)
     zip_c =  85200
         
     names = ["Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Henry", "Ivy", "Jack",
@@ -139,7 +120,7 @@ def insert_data_agent(conn, zip_codes):
 
     cur = conn.cursor()
     for i in range (0, zip_codes):
-        for _ in range(1200):  # Create 4 agents for each zip code
+        for _ in range(agent_per_zipcode):  # Create 4 agents for each zip code
             name = random.choice(names)
             insert_data_query = f"INSERT INTO {DELIVERY_AGENT_TABLE} ({AGENT_NAME},{ORDER_ID},{ZIPCODE}) VALUES ('{name}',NULL,{zip_c+i}) RETURNING {AGENT_ID}"
             cur.execute(insert_data_query)
@@ -147,7 +128,7 @@ def insert_data_agent(conn, zip_codes):
     conn.commit()
 
 
-def make_inventory(conn, zipcodes):
+def create_partition_on_inventory_table(conn, zipcodes):
     print("Creating Inventory table...")
     zip_codes = min(100, zipcodes)
 
@@ -158,97 +139,54 @@ def make_inventory(conn, zipcodes):
     CREATE TABLE Inventory (
         {UUID} serial,
         {WAREHOUSE_ID} INT NOT NULL,
-        {ORDER_ID} VARCHAR(10),
+        {ORDER_ID} INT,
         {MED_ID} INT NOT NULL,
         {ZIPCODE} INT NOT NULL,
         PRIMARY KEY ( {ZIPCODE}, {MED_ID}, {UUID})
-    ) PARTITION BY LIST ({ZIPCODE});
-                               """
+    ) PARTITION BY LIST ({ZIPCODE});"""
 
     cur.execute(table_creation_query)
-    
-
     zip_c =  85200
     for i in range (0, zip_codes):
         partition_creation_query = f"CREATE TABLE Inventory_zip_code_{zip_c+i} PARTITION OF {INVENTORY} FOR VALUES IN ({zip_c+i});"
         cur.execute(partition_creation_query)
-        # create index if not exists
-        index_creation_query = f"CREATE INDEX IF NOT EXISTS Inventory_zip_code_{zip_c+i}_index ON Inventory_zip_code_{zip_c+i} ({MED_ID});"
-        # cur.execute(index_creation_query)
-        
-
     conn.commit()
 
 
-def insert_data_inventory(conn, zip_codes, warehouse, medicine, rows_inventory):
+def insert_data_inventory(conn, zip_codes, warehouse, medicine):
     print("Inserting data into Inventory table...")
     zip_codes = min(6, zip_codes)
-    # zip_list = [f"{i:02}" for i in range(zip_codes)]
     warehouse_list = list(range(1, warehouse + 1))
     medicine_list = list(range(1, medicine + 1))
     cur = conn.cursor()
     zip_c =  85200
     for i in range (0, zip_codes):
         for medi in medicine_list:
-            for _ in range(1400):  # Ensure 10 counts for each med_id
+            for _ in range(stock_per_warehouse):  # Ensure 10 counts for each med_id
                 wh = random.choice(warehouse_list)
                 insert_data_query = f"INSERT INTO {INVENTORY} ({WAREHOUSE_ID},{ORDER_ID},{MED_ID},{ZIPCODE}) VALUES ({wh},NULL,{medi},{zip_c+i})"
                 cur.execute(insert_data_query)
 
     conn.commit()
 
-def create_sql_function(conn):
-    # todo: add more sql procedures for without partition cluse and with partition clause, also without skip locked and with skip locked
-    cursor = conn.cursor()
-    cursor.execute("""
-        CREATE OR REPLACE FUNCTION update_inventory(order_id_param INT, med_id_param INT, zip_code_param INT, quantity_param INT)
-            RETURNS INT AS
-            $$
-            DECLARE
-                row_count INT;
-                updated_count INT;
-            BEGIN
-                WITH selected_rows AS (
-                    SELECT uuid
-                    FROM inventory
-                    WHERE med_id = med_id_param AND order_id IS NULL AND zip_code = zip_code_param
-                    ORDER BY uuid
-                    LIMIT quantity_param
-                    FOR UPDATE skip locked
-                ),
-                updated_rows AS (
-                    UPDATE inventory 
-                    SET order_id = order_id_param
-                    WHERE uuid IN (SELECT uuid FROM selected_rows)
-                    RETURNING uuid
-                )
-                SELECT COUNT(*) INTO updated_count FROM updated_rows;
-                RETURN updated_count;
-            END;
-            $$ LANGUAGE plpgsql;
-            """)
-    conn.commit()
-
-
 if __name__ == '__main__':
     zipcodes = 10
     warehouse = 10
-    medicine = 10
-    rows_inventory = 1000;
-    print("hello tables...")
+    medicine = 100
+    agent_per_zipcode = 5
+    stock_per_warehouse = 30
     try:
-
         conn = psycopg2.connect(database="postgres", user="postgres", host='localhost', password="postgres", port=5432)
         create_database(DATABASE, conn)
         conn = psycopg2.connect(database=DATABASE, user="postgres", host='localhost', password="postgres", port=5432)
         print(conn)
-        make_order(conn, zipcodes)
-        make_order_item(conn, zipcodes)
-        make_delivery_agent(conn, zipcodes)
+        create_partition_on_order_table(conn, zipcodes)
+        create_partition_on_orderitem_table(conn, zipcodes)
+        create_partition_on_deliveryagent_table(conn, zipcodes)
         insert_data_agent(conn, zipcodes)
-        make_inventory(conn, zipcodes)
-        insert_data_inventory(conn, zipcodes, warehouse, medicine, rows_inventory)
-        create_sql_function(conn)
+        create_partition_on_inventory_table(conn, zipcodes)
+        insert_data_inventory(conn, zipcodes, warehouse, medicine)
+
 
         conn.close()
     except Exception as e:
